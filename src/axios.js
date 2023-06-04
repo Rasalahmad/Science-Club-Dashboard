ebc86bb7b2cadf0e2092178450acb51feba2a708
Rasalahmad <rasalahmadsit@gmail.com>
2023-06-04 21:59:22 +0600

    wip

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,12 @@
 import axios from "axios";
 
 export const makeRequest = axios.create({
-  baseURL: "https://club.hotelgreenlandbd.com/api",
+  baseURL:
+    process.env.REACT_APP_PRODUCTION === "YES"
+      ? "https://club.hotelgreenlandbd.com/api"
+      : "http://localhost:5000/api",
   withCredentials: true,
 });
 
 // http://localhost:5000/api
+// https://club.hotelgreenlandbd.com/api
