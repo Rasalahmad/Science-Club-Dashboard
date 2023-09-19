84820fb617ac249101c0bf7f92871208037fc488
Rasalahmad <rasalahmadsit@gmail.com>
2023-09-19 18:34:34 +0600

    wip

--- src/components/datatable/ResultDataTable.jsx
+++ src/components/datatable/ResultDataTable.jsx
@@ -1,8 +1,6 @@
 import "./datatable.scss";
 import { DataGrid } from "@mui/x-data-grid";
-import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
-import Swal from "sweetalert2";
 import { makeRequest } from "../../axios";
 import { resultColumn } from "../../datatablesource";
 
